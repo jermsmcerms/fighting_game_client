5bd9d6e154f685d4afd5087631b6e492b3a6f781
jeremycbrothers <jeremycbrothers@gmail.com>
2021-10-19 16:12:04 -0400

    uploading everything else

---
+++ desktop/src/com/rose/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.rose;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rose.main.Rose;
+
+public class DesktopLauncher {
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 800;
+        config.height = 480;
+        new LwjglApplication(new Rose(), config);
+    }
+}
